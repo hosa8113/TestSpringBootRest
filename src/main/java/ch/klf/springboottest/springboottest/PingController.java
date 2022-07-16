a712779fa63e8fd28df8240aab862ab23caf7dc7
sam <samuel.hofmann@klf.ch>
2022-07-16 19:43:23 +0200

    initial commit

---
+++ src/main/java/ch/klf/springboottest/springboottest/PingController.java
@@ -0,0 +1,17 @@
+package ch.klf.springboottest.springboottest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping(value = "/ping")
+    @ResponseStatus(HttpStatus.OK)
+    public String ping() {
+        return "pong";
+    }
+
+}
---
+++ src/test/java/ch/klf/springboottest/springboottest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.klf.springboottest.springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
